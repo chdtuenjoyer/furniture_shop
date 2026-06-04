0878ad7d76a649bc1e0a9946ab8ffcaa01dd8558
Olexandr Lytvyn <o.s.lytvyn.fitis23@chdtu.edu.ua>
2026-06-04 19:35:51 +0300

    fix4

--- server/index.js
+++ server/index.js
@@ -412,10 +412,7 @@ app.post('/api/orders', authorize, async (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('/*', (req, res) => {
-    if (req.path.startsWith('/api/')) {
-      return res.status(404).json({ error: 'Not found' });
-    }
+  app.get(/^(?!\/api\/).*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
